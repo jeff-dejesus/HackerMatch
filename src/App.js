80468c2325ff29b0bbcade84e0a0f5dfdacaa55f
Jeffrey de Jesus <dejesus.y.jeffrey@gmail.com>
2021-02-13 12:42:25 -0800

    Commit files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { userAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp( {
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
