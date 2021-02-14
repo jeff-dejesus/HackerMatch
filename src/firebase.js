b7bf7ca03e1e517aa65bdf2047b2d00143036389
Jeffrey de Jesus <dejesus.y.jeffrey@gmail.com>
2021-02-13 18:53:59 -0800

    successful account creation

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyA5FykG90fqYYtuIixgzrgziCA-fatiHgg",
+  authDomain: "celtic-artwork-296608.firebaseapp.com",
+  projectId: "celtic-artwork-296608",
+  storageBucket: "celtic-artwork-296608.appspot.com",
+  messagingSenderId: "156152026820",
+  appId: "1:156152026820:web:ffcf741d35492e0d99db91",
+  measurementId: "G-5X41MLCHN9"
+})
+
+export const auth = app.auth()
+export default app
